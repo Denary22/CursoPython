92ca43f59bba2140bf8815225b4941e1eb197ee2
Denary22 <89047084+Denary22@users.noreply.github.com>
2022-06-30 15:04:47 -0500

    Create Ejercicio02.py
    Crear una lista y mostrarla

---
+++ Ejercicio02.py
@@ -0,0 +1,7 @@
+#Reyes Lugo Luz Daniela
+#Ejercicio 02
+
+#Escribir un programa que almacene las asignaturas de un curso en una lista y la muestre por pantalla.
+
+lista = ["Matemáticas", "Español", "Física", "Química", "Historia" , "Lengua"]
+print(lista)
