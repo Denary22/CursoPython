e2c605a179c7c77acd8e6a648ff4fcd9696d63a8
Daniela Reyes <danielareyes220399@gmail.com>
2022-07-14 20:16:19 -0500

    Create Ejercicio05.py

---
+++ Ejercicio05.py
@@ -0,0 +1,31 @@
+#Ejercicio 05: Reloj
+#Reyes Lugo Luz Daniela
+
+import os
+import time
+
+entrada = input("Ingrese la hora hora:minutos:segundos de esta manera: ")
+lista = entrada.split(":") ## 5:12:41 - ["5", "12", "41"]
+print(lista)
+e_hora = int(lista [0])
+e_minuto = int(lista[1])
+e_segundo = int(lista[2])
+
+for hora in range(e_hora, 24):
+    for minuto in range(e_minuto, 60):
+        for segundo in range(e_segundo, 60):
+            os.system("cls")
+            print(f"{hora}:{minuto}:{segundo}")
+            time.sleep(1)
+            
+            if segundo == 59:
+                segundo = 0
+                e_segundo = 0
+        
+        if minuto == 59:
+            minuto = 0
+            e_minuto = 0 
+        
+    if hora == 23:
+        hora = 0
+        e_hora = 0
